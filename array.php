7b7891882071d042fb87218f5f07674075721ff6
Ashii99 <124756780+ashimaro@users.noreply.github.com>
2023-04-01 22:47:13 +0800

    array and print output

---
+++ array.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+$hobby = array("coding", "drawing", "read books"); 
+echo "I like " . $hobby[0] . ", " . $hobby[1] . " and " . $hobby[2] . ".";
+echo "(Number of hobbies : " . count($hobby) . ")";
+?>
+</body>
+</html>
