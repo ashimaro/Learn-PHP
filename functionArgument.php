2d919b7c218eac6d9a857b0669615115c0fe2e20
Ashii99 <124756780+ashimaro@users.noreply.github.com>
2023-04-02 20:15:06 +0800

    a function with two arguments ($name and $year):

---
+++ functionArgument.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+function cars($name, $year) {
+  echo "Honda $name . Since $year <br>";
+}
+
+cars("Civic","1975");
+cars("City","1978");
+cars("Jazz","1983");
+?>
+
+</body>
+</html>
