1a853444caacba837f5577606a4cd671bc262855
Ashii99 <124756780+ashimaro@users.noreply.github.com>
2023-04-04 22:25:44 +0800

    SwitchStatement

---
+++ switchStatement.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$favcolor = "mg3";
+
+switch ($favcolor) {
+  case "mg3":
+    echo "You choose Myvi Advance!";
+    break;
+  case "honda":
+    echo "You choose Honda Civic";
+    break;
+  case "gtr":
+    echo "You choose GTR!";
+    break;
+  default:
+    echo "You didn't choose any cars!";
+}
+?>
+ 
+</body>
+</html>
