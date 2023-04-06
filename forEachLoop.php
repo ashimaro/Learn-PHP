de491c26dea5a39cfceee1a9a339592012489e16
Ashii99 <124756780+ashimaro@users.noreply.github.com>
2023-04-06 21:23:13 +0800

    forEachLoop

---
+++ forEachLoop.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$price = array("Honda"=>"70k", "Mustang"=>"100k", "Myvi"=>"60k");
+
+foreach($price as $x => $val) {
+  echo "$x = $val<br>";
+}
+?>
+
+</body>
+</html>
