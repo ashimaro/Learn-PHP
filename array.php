4c0c73ac1380c77e956486d90584b3b59d599717
Ashii99 <124756780+ashimaro@users.noreply.github.com>
2023-04-24 22:18:57 +0800

    add comment

--- array.php
+++ array.php
@@ -3,8 +3,8 @@
 <body>
 <?php
 $hobby = array("coding", "drawing", "read books"); 
-echo "I like " . $hobby[0] . ", " . $hobby[1] . " and " . $hobby[2] . ".";
-echo "(Number of hobbies : " . count($hobby) . ")";
+echo "I like " . $hobby[0] . ", " . $hobby[1] . " and " . $hobby[2] . "."; // output the list of hobbies
+echo "(Number of hobbies : " . count($hobby) . ")"; // output the number of hobbies in the array
 ?>
 </body>
 </html>
