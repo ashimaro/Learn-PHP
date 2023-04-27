b06e5bae0e6924beb3e42ca5856677de65e3ece7
Ashii99 <124756780+ashimaro@users.noreply.github.com>
2023-04-27 23:46:03 +0800

    add comment

--- forLoop.php
+++ forLoop.php
@@ -6,6 +6,14 @@
 for ($x = 1; $x <= 10; $x++) {
   echo "The number is: $x <br>";
 }
+//for (statement 1; statement 2; statement 3) {
+  // code block to be executed}
+
+//Statement 1 is executed (one time) before the execution of the code block.
+
+//Statement 2 defines the condition for executing the code block.
+
+//Statement 3 is executed (every time) after the code block has been executed.
 ?>  
 
 </body>
