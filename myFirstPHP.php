864400712ee76259e9cc4ab40a5217be0622d634
Ashii99 <124756780+ashimaro@users.noreply.github.com>
2023-05-02 23:47:02 +0800

    add comment

--- myFirstPHP.php
+++ myFirstPHP.php
@@ -1,10 +1,11 @@
 <!DOCTYPE html>
 <html>
 <body>
-
 <?php
 $txt = "Freelance projects makes me take another step forward and learn PHP";
 echo "Hello World! $txt!";
+
+// echo is used to print text . (SOP in java)
 ?>
 
 </body>
